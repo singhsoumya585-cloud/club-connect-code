d3d29c6ecc15428117856706068fe8e31e97bc93
singhsoumya585-cloud <singhsoumya585@gmail.com>
2026-01-15 20:32:34 -0600

    Update Validators.java

--- utils/Validators.java
+++ utils/Validators.java
@@ -1,1 +1,19 @@
+package utils;
 
+
+public class Validators {
+
+   
+    public static boolean validateUsername(String username) {
+        return username != null && username.length() > 3;
+    }
+
+    public static boolean validateEmail(String email) {
+        return email != null && email.contains("@") && email.contains(".");
+    }
+
+  
+    public static boolean validatePassword(String password) {
+        return password != null && password.length() >= 6;
+    }
+}
