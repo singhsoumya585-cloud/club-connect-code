fac58cf751aa811faf166e2d9f0b46a9ab39e897
singhsoumya585-cloud <singhsoumya585@gmail.com>
2026-01-15 20:24:18 -0600

    Update DatabaseClient.java

--- database/DatabaseClient.java
+++ database/DatabaseClient.java
@@ -1,1 +1,9 @@
+package database;
+
+public interface DatabaseClient {
+
+    boolean validateCredentials(String username, String password);
+
+    void createUser(String username, String password, String email);
+}
 
