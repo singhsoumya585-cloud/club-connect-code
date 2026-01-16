cf9d92f47ae06d810b531fe13b2fef9867e6f7f6
singhsoumya585-cloud <singhsoumya585@gmail.com>
2026-01-15 20:32:56 -0600

    Update Logger.java

--- utils/Logger.java
+++ utils/Logger.java
@@ -1,1 +1,13 @@
+package utils;
+
+import java.time.LocalDateTime;
+
+
+public class Logger {
+
+  
+    public static void log(String message) {
+        System.out.println("[" + LocalDateTime.now() + "] " + message);
+    }
+}
 
