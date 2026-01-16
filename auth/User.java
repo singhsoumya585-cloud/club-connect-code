2c4b46d1d1a5d677160830d10c2603df9b048ea6
singhsoumya585-cloud <singhsoumya585@gmail.com>
2026-01-15 20:19:25 -0600

    Update User.java

--- auth/User.java
+++ auth/User.java
@@ -1,1 +1,22 @@
+public class User {
+    private String userId;
+    private String username;
+    private String password;
+    private boolean isAdmin;
+
+    public User(String userId, String username, String password, boolean isAdmin) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+}
 
