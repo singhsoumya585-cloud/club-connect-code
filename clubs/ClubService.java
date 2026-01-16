be9a8bd8021150592309f7205a81ce4fd423b5dd
singhsoumya585-cloud <singhsoumya585@gmail.com>
2026-01-15 20:28:52 -0600

    Update ClubService.java

--- clubs/ClubService.java
+++ clubs/ClubService.java
@@ -1,1 +1,23 @@
 
+package clubs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClubService {
+
+    private List<Club> clubs = new ArrayList<>();
+
+    public ClubService() {
+        clubs.add(new Club("1", "Chess Club"));
+        clubs.add(new Club("2", "Robotics Club"));
+    }
+
+    public List<Club> getClubs() {
+        return clubs;
+    }
+
+    public void joinClub(Club club) {
+        club.addMember();
+    }
+}
