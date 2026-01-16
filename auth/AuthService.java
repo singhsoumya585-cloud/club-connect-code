acd05532985bb65990fabbe574bfc5066a864d2e
singhsoumya585-cloud <singhsoumya585@gmail.com>
2026-01-15 20:22:45 -0600

    Update AuthService.java

--- auth/AuthService.java
+++ auth/AuthService.java
@@ -1,1 +1,20 @@
+package auth;
 
+import database.DatabaseClient;
+
+public class AuthService {
+
+    private DatabaseClient database;
+
+    public AuthService(DatabaseClient database) {
+        this.database = database;
+    }
+
+    public boolean login(String username, String password) {
+        return database.validateCredentials(username, password);
+    }
+
+    public void register(String username, String password, String email) {
+        database.createUser(username, password, email);
+    }
+}
