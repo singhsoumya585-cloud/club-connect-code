0756d3092b5f1f65567a9ca048a6f8e45fd7096a
singhsoumya585-cloud <singhsoumya585@gmail.com>
2026-01-15 20:23:44 -0600

    Update AccountRecoveryService.java

--- auth/AccountRecoveryService.java
+++ auth/AccountRecoveryService.java
@@ -1,1 +1,10 @@
+package auth;
+
+public class AccountRecoveryService {
+
+    public void initiateRecovery(String email) {
+        // Logic to trigger recovery workflow
+        System.out.println("Recovery instructions sent to " + email);
+    }
+}
 
