15a3bba48748211ea4f5ec40d6a098aab21a6f16
singhsoumya585-cloud <singhsoumya585@gmail.com>
2026-01-15 20:25:05 -0600

    Update Firestore.java

--- database/Firestore.java
+++ database/Firestore.java
@@ -1,1 +1,20 @@
 
+package database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Firestore implements DatabaseClient {
+
+    private Map<String, String> users = new HashMap<>();
+
+    @Override
+    public boolean validateCredentials(String username, String password) {
+        return users.containsKey(username) && users.get(username).equals(password);
+    }
+
+    @Override
+    public void createUser(String username, String password, String email) {
+        users.put(username, password);
+    }
+}
