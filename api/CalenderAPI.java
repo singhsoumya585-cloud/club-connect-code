e8594531bc4c19eb5142011ea4f784890f44704d
singhsoumya585-cloud <singhsoumya585@gmail.com>
2026-01-15 20:29:24 -0600

    Update CalenderAPI.java

--- api/CalenderAPI.java
+++ api/CalenderAPI.java
@@ -1,1 +1,9 @@
 
+package api;
+
+public class CalendarAPI {
+
+    public void addEvent(String clubName, String date) {
+        System.out.println("Event added for " + clubName + " on " + date);
+    }
+}
