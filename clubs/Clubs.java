9d6390ec2914a835a89dac4e77ddb7a870eff350
singhsoumya585-cloud <singhsoumya585@gmail.com>
2026-01-15 20:25:36 -0600

    Update Clubs.java

--- clubs/Clubs.java
+++ clubs/Clubs.java
@@ -1,1 +1,22 @@
+package clubs;
+
+public class Club {
+    private String id;
+    private String name;
+    private int memberCount;
+
+    public Club(String id, String name) {
+        this.id = id;
+        this.name = name;
+        this.memberCount = 0;
+    }
+
+    public void addMember() {
+        memberCount++;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
 
